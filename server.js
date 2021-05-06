1eabbe525379d7e47363441593ce53ca2b45041e
rajaosama <raja.o@atompoint.com>
2021-05-06 12:47:01 +0500

    C

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const next = require("next");
 const port = process.env.PORT || 3000;
-const dev = true;
+const dev = false;
 const app = next({ dev });
 const handle = app.getRequestHandler();
 const server = express();
